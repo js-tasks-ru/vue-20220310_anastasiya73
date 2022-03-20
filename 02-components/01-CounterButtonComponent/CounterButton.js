575abe5341b67c72e5010a75a84a69aa92a9ad66
Natalia.Kosheutova <Natalia.Kosheutova@nordigy.ru>
2022-03-20 21:00:40 +0100

    bug fixing

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -2,7 +2,12 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'CounterButton',
-  props: ['count'],
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
   methods: {
     increase() {
       this.$emit('update:count', this.count + 1);
