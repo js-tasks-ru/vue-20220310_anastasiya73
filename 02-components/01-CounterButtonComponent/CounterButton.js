9bb43430e74ac5fa22a618d4b64391feda4ecc6b
Natalia.Kosheutova <Natalia.Kosheutova@nordigy.ru>
2022-03-20 20:57:41 +0100

    added CounterButtonComponent

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -2,11 +2,11 @@ import { defineComponent } from './vendor/vue.esm-browser.js';
 
 export default defineComponent({
   name: 'CounterButton',
-
-  // Компонент должен иметь входной параметр
-
-  // Шаблон лучше держать максимально простым, а логику выносить в методы
-
-  // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  props: ['count'],
+  methods: {
+    increase() {
+      this.$emit('update:count', this.count + 1);
+    },
+  },
+  template: `<button @click="increase" type="button">{{ count }}</button>`,
 });
